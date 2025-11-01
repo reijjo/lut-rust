dda6858ffecc79d07aac7398372cc4e3a5c0d2d0
Reijjo <teemu.aitomeri@gmail.com>
2025-11-01 21:51:41 +0200

    rust server started

--- course_project/server/src/main.rs
+++ course_project/server/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+	routing::get,
+	Router
+};
+
+#[tokio::main]
+async fn main() {
+	let app = Router::new().route("/", get(|| async { "hello world!"}));
+
+	let listener = tokio::net::TcpListener::bind("localhost:3001").await.unwrap();
+	axum::serve(listener, app).await.unwrap()
 }
