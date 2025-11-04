c24c178411f864767dc659e8fa54a0e0191c76f2
Reijjo <teemu.aitomeri@gmail.com>
2025-11-04 20:14:05 +0200

    homepage 100% responsive

--- course_project/client/src/components/layout/navbar/Navbar.tsx
+++ course_project/client/src/components/layout/navbar/Navbar.tsx
@@ -4,6 +4,10 @@ import { usePathname } from "next/navigation";
 import "./Navbar.css";
 import Link from "next/link";
 import NavCart from "./NavCart";
+import Button from "@/components/ui/button/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faClose } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const navLinks = [
   {
@@ -28,8 +32,32 @@ const navLinks = [
   },
 ];
 
-export default function Navbar() {
+type NavLinksProps = {
+  setMobileOpen: (isOpen: boolean) => void;
+};
+
+export const NavLinks = ({ setMobileOpen }: NavLinksProps) => {
   const path = usePathname();
+
+  return (
+    <ul className="nav-links">
+      {navLinks.map((link) => (
+        <li key={link.name}>
+          <Link
+            href={link.link}
+            className={path === link.link ? "active" : ""}
+            onClick={() => setMobileOpen(false)}
+          >
+            {link.name}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default function Navbar() {
+  const [mobileOpen, setMobileOpen] = useState(false);
 
   return (
     <nav>
@@ -37,20 +65,20 @@ export default function Navbar() {
         <Link href="/" className="nav-logo">
           Logo
         </Link>
-        <ul className="nav-links">
-          {navLinks.map((link) => (
-            <li key={link.name}>
-              <Link
-                href={link.link}
-                className={path === link.link ? "active" : ""}
-              >
-                {link.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <NavLinks setMobileOpen={setMobileOpen} />
+        <Button
+          className="btn-secondary nav-hamburger"
+          onClick={() => setMobileOpen(!mobileOpen)}
+        >
+          <FontAwesomeIcon icon={mobileOpen ? faClose : faBars} />
+        </Button>
         <NavCart />
       </div>
+      {mobileOpen && (
+        <div className="mobile-menu">
+          <NavLinks setMobileOpen={setMobileOpen} />
+        </div>
+      )}
     </nav>
   );
 }
