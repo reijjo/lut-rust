09be844982300709eed6f50ea875352346c93b68
Reijjo <teemu.aitomeri@gmail.com>
2025-11-09 14:13:06 +0200

    added zustand for navbar cart total

--- course_project/client/src/components/layout/navbar/NavCart.tsx
+++ course_project/client/src/components/layout/navbar/NavCart.tsx
@@ -3,21 +3,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 import { formatPrice } from "@/utils/formatters";
-import { use } from "react";
-import { Cart } from "@/utils/types";
+import { useCartTotal } from "@/lib/stores/cartStore";
 
-type NavCartProps = {
-  cart: Promise<Cart>;
-};
-
-export default function NavCart({ cart }: NavCartProps) {
-  const cartTotal = use(cart);
+export default function NavCart() {
+  const total = useCartTotal((state) => state.total);
 
   return (
     <div className="nav-cart">
       <Link href="/cart" className="nav-cart-content">
         <FontAwesomeIcon icon={faCartShopping} />
-        {formatPrice(cartTotal.total)}
+        {formatPrice(total)}
       </Link>
     </div>
   );
